5ef682057f37c0ec682f3c1050e8267c8d4974c7
Jin Wu <jinn.wuu@gmail.com>
2020-01-09 16:35:17 -0500

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,41 @@
+require_relative 'transfer'
+
 class BankAccount
 
+    attr_reader :name, :deposit, :display_balance
+    attr_accessor :balance, :status
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @balance = 1000
+        @status = "open"
+
+        @@all << self
+    end
+
+    def deposit(deposit)
+        self.balance += deposit
+    end
+
+    def display_balance
+        "Your balance is $" + balance.to_s + "."
+    end
+  
+    def valid?
+        if self.status == "open" && self.balance > 0
+            true
+        else
+            false
+        end
+    end
+
+    def self.all
+        @self
+    end
+
+    def close_account
+        self.status = "closed"
+    end
 end
